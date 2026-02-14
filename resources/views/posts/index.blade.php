6f01ce57f822fd2bb1bcc571de213c6b11cc69c1
kokona yano <coco1210@ymail.ne.jp>
2026-02-15 03:44:03 +0900

    kadai

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,8 @@
 <h1>投稿一覧</h1>
+
+@foreach ($posts as $post)
+    <div style="margin-bottom: 20px;">
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->content }}</p>
+    </div>
+@endforeach
